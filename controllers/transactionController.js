6b0e45578d11f5a47aaae9cd4bafd8717741bfb0
Sresta Malla <mallasresta@gmail.com>
2023-03-02 16:36:28 +0545

    [task] database connected and created model

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,7 +1,25 @@
+const TransactionModel = require("../models/TransactionModel");
+
 //@desc Get all transactions
 //@route GET/api/v1/transaction
 //access public
 
 exports.getTransactions = (req, res, next) => {
   res.send("Get transactions");
+};
+
+//@desc Add transactions
+//@route POST/api/v1/transaction
+//access public
+
+exports.addTransaction = (req, res, next) => {
+  res.send("POST transaction");
+};
+
+//@desc Delete transactions
+//@route DELETE/api/v1/transaction
+//access public
+
+exports.deleteTransaction = (req, res, next) => {
+  res.send("DELETE transaction");
 };
--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getTransactions } = require("../controllers/transactionController");
+const {
+  getTransactions,
+  deleteTransaction,
+  addTransaction,
+} = require("../controllers/transactionController");
 
-router.route("/").get(getTransactions);
-
+router.route("/").get(getTransactions).post(addTransaction);
+router.route("/:id").delete(deleteTransaction);
 module.exports = router;
